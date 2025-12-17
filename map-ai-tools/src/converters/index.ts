d8aae0c811da0c1cd97c429e8a5313dabd7b02de
menusal <daniarlandis@gmail.com>
2025-12-17 15:50:07 +0100

    wip: deck-schema

---
+++ map-ai-tools/src/converters/index.ts
@@ -0,0 +1,40 @@
+/**
+ * Spec Converters
+ *
+ * Export all spec generator functions for @deck.gl/json output
+ */
+
+export {
+  // View state generators
+  generateFlyToSpec,
+  generateZoomSpec,
+  generateViewStateSpec,
+  type FlyToParams,
+  type ZoomMapParams,
+  type SetViewStateParams,
+  // Layer operation generators
+  generateToggleLayerSpec,
+  generateSetPointColorSpec,
+  generateColorByPropertySpec,
+  generateFilterSpec,
+  generateSizeByPropertySpec,
+  type ToggleLayerParams,
+  type SetPointColorParams,
+  type ColorFeaturesByPropertyParams,
+  type FilterFeaturesByPropertyParams,
+  type SizeFeaturesByPropertyParams,
+  // Layer management generators
+  generateAddLayerSpec,
+  generateAddRasterLayerSpec,
+  generateRemoveLayerSpec,
+  generateUpdateLayerPropsSpec,
+  type AddLayerParams,
+  type AddRasterLayerParams,
+  type RemoveLayerParams,
+  type UpdateLayerPropsParams,
+  // Utilities
+  mergeSpecs,
+  hasViewStateChanges,
+  hasLayerChanges,
+  getAffectedLayerIds,
+} from './spec-generator';
