1b834fcd7cdbe16383f53103ebb635f487ffb7af
menusal <daniarlandis@gmail.com>
2026-01-15 18:36:08 +0100

    feat: rename folders

--- integration-examples/sdsc-2025-congestion/layers/blending.js
+++
@@ -1,9 +0,0 @@
-export const ADDITIVE_BLEND_PARAMETERS = {
-  blendColorSrcFactor: "src-alpha",
-  blendAlphaSrcFactor: "src-alpha",
-  blendColorDstFactor: "dst-alpha",
-  blendAlphaDstFactor: "dst-alpha",
-  blendColorOperation: "add",
-  blendAlphaOperation: "add",
-  //depthTest: false,
-};
