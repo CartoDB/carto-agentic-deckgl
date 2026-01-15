2ab6fe866a981e20e401c7e3760aca575594341c
menusal <daniarlandis@gmail.com>
2026-01-15 18:36:08 +0100

    feat: rename folders

--- integration-examples/sdsc-2025-congestion/components/Chat/index.js
+++
@@ -1,7 +0,0 @@
-export { ChatButton } from './ChatButton';
-export { ChatUI } from './ChatUI';
-export { ChatPanel } from './ChatPanel';
-export { ToolLoader } from './ToolLoader';
-// Legacy exports for backwards compatibility
-export { ChatFAB } from './ChatFAB';
-export { FloatingChatWindow } from './FloatingChatWindow';
